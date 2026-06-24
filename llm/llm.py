ccf98a6b0fadf31a86f8be187a9024613d493c74
Deepanshu Rawat <dr866699@gmail.com>
2026-06-25 00:49:52 +0530

    prepare deployment

--- llm/llm.py
+++ llm/llm.py
@@ -1,14 +1,31 @@
+import os
+
 from langchain_ollama import ChatOllama
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 
 def get_llm():
-    """
-    Initialize and return the LLM.
-    """
 
-    llm = ChatOllama(
-        model="llama3.1",
-        temperature=0
+    llm_provider = os.getenv(
+        "LLM_PROVIDER",
+        "ollama"
     )
+
+    if llm_provider == "ollama":
+
+        llm = ChatOllama(
+            model="llama3.1",
+            temperature=0
+        )
+
+    else:
+
+        llm = ChatGoogleGenerativeAI(
+            model="gemini-2.5-flash",
+            google_api_key=os.getenv(
+                "GOOGLE_API_KEY"
+            ),
+            temperature=0
+        )
 
     return llm
--- rag/embeddings.py
+++ rag/embeddings.py
@@ -1,7 +1,16 @@
-from langchain_ollama import OllamaEmbeddings
+# from langchain_ollama import OllamaEmbeddings
+
+
+# def get_embeddings():
+#     return OllamaEmbeddings(
+#         model="nomic-embed-text"
+#     )
+
+from langchain_huggingface import HuggingFaceEmbeddings
 
 
 def get_embeddings():
-    return OllamaEmbeddings(
-        model="nomic-embed-text"
+
+    return HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
